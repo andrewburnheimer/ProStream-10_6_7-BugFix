7dd41e600204bee89bb6b155eb1c913b373056fb
Andrew Burnheimer <Andrew.Burnheimer@nbcuni.com>
2016-09-05 20:16:34 -0400

    Initial commit

---
+++ ProStream-10_6_7-BugWorkaround/BugWorkaround.vb
@@ -0,0 +1,68 @@
+﻿Module BugWorkaround
+
+    Sub Main()
+        Try
+            Dim Filename As String = ""
+            Dim args = My.Application.CommandLineArgs
+            If args.Count() > 0 Then
+                Filename = args.Item(0).ToString
+            Else
+                Throw New MissingArgumentException("Please run on console with a filename argument, or drag and drop an input file on icon or shortcut.")
+            End If
+
+
+            Dim ReadHandle As IO.StreamReader = FileIO.FileSystem.OpenTextFileReader(Filename)
+            'could be a big file...
+            Dim FileText As String = ReadHandle.ReadToEnd()
+            ReadHandle.Close()
+
+            Dim NumOccurences As Integer = 0
+            NumOccurences = FindWords(FileText, "RegenerateTables=""0""")
+            Dim Replaced As String = Replace(FileText, "RegenerateTables=""0""", "RegenerateTables=""1""")
+
+            If FindWords(Replaced, "RegenerateTables=""0""") > 0 Then
+                Throw New ConstraintException("Currently written to set all occurences")
+            End If
+
+            Dim FilenameInfo As System.IO.FileInfo
+            FilenameInfo = My.Computer.FileSystem.GetFileInfo(Filename)
+            Dim FileFolderPath As String = FilenameInfo.DirectoryName
+            Dim ChangedFilename As String = Replace(FilenameInfo.Name, ".xml", "-RegenTables.xml")
+
+            Dim WriteHandle As IO.StreamWriter = FileIO.FileSystem.OpenTextFileWriter(FileFolderPath & "\" & ChangedFilename, False)
+            WriteHandle.Write(Replaced)
+            WriteHandle.Close()
+
+            Console.Out().WriteLine("Changed " & NumOccurences & " occurences In " & Filename)
+        Catch e As MissingArgumentException
+            Console.Out().WriteLine(e.Message)
+        Finally
+            Console.Out().WriteLine("Press Enter to continue...")
+            Console.ReadLine()
+        End Try
+
+    End Sub
+
+    ' From http://www.visual-basic-tutorials.com/Tutorials/Strings/count-how-many-times-a-string-occurs-in-visual-basic.htm
+    Private Function FindWords(ByVal TextSearched As String, ByVal Paragraph As String) As Integer
+        Dim location As Integer = 0
+        Dim occurances As Integer = 0
+
+        Do
+            location = TextSearched.IndexOf(Paragraph, location)
+            If location <> -1 Then
+                occurances += 1
+                location += Paragraph.Length
+            End If
+
+        Loop Until location = -1
+        Return occurances
+    End Function
+    Class MissingArgumentException
+        Inherits ConstraintException
+        Public Sub New(s As String)
+            MyBase.New(s)
+        End Sub
+    End Class
+
+End Module
---
+++ ProStream-10_6_7-BugWorkaround/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("ProStream-10_6_7-BugWorkaround")>
+<Assembly: AssemblyDescription("Console application to quickly set RegenerateTables setting for all ServiceOut tags in ProStream config. backup files")>
+<Assembly: AssemblyCompany("NBCUniversal")>
+<Assembly: AssemblyProduct("ProStream-10_6_7-BugWorkaround")>
+<Assembly: AssemblyCopyright("Copyright ©  2016")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("9b1420dc-1782-4636-91d1-bb613e1365c5")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
